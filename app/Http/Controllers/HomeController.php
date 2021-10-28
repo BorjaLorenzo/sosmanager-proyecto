70da0b0f02a30f8e5768d9230be2a35c332419bf
sanderfakes <borjalb98@gmail.com>
2021-10-28 18:24:39 +0200

    commit 4

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,19 +24,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-       $rol=Auth::id();
-       
-        return view('menu_trabajador',['rol'=>$rol]);
-        // switch ($rol) {
-        //     case 'T':
-        //         return view('menu_adm',['rol'=>$rol]);
-        //         break;
-        //     case 'E':
-        //         return view('menu_encargado');
-        //         break;
-        //     case 'A':
-        //         return view('menu_adm');
-        //         break;
-        // }
+       $rol=Auth::user();
+
+        return view('menu',['rol'=>$rol]);
+        
     }
 }
